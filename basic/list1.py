15d292354a05589e1f327abb831538688f520976
renzon <renzon@gmail.com>
2017-05-16 10:42:24 -0300

    implemented sort_lastˆ

--- basic/list1.py
+++ basic/list1.py
@@ -21,7 +21,7 @@
 # strings where the string length is 2 or more and the first
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
-from itertools import chain
+import operator
 
 
 def match_ends(words):
@@ -57,8 +57,7 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    # +++your code here+++
-    return
+    return sorted(tuples, key=operator.itemgetter(-1))
 
 
 # Simple provided test() function used in main() to print
