e0051720eccd458a8f0a9c923d3441b6a9c95cf7
renzon <renzon@gmail.com>
2017-05-16 14:26:00 -0300

    implemented mix_upˆ

--- basic/string1.py
+++ basic/string1.py
@@ -56,8 +56,7 @@ def fix_start(s):
     iter_s = iter(s)
     first_char = next(iter_s)
 
-    def char_or_asterisc(c):
-        return '*' if c == first_char else c
+    def char_or_asterisc(c):        return '*' if c == first_char else c
 
     return ''.join(chain(first_char, map(char_or_asterisc, iter_s)))
 
@@ -70,8 +69,7 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-    # +++your code here+++
-    return
+    return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
