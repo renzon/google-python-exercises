ac6c676d39dedb75143c27f942f531cda6316494
renzon <renzon@gmail.com>
2017-06-03 01:30:58 -0300

    Second solution: alternating states

--- codingbat/sum67.py
+++ codingbat/sum67.py
@@ -1,3 +1,6 @@
+from functools import partial
+
+
 def sum67(nums):
     """Solution to the problem described in http://codingbat.com/prob/p108886
 
@@ -23,4 +26,39 @@ def sum67(nums):
         elif n == 7:
             between_6_and_7 = False
 
+    return sum
+
+
+def sum67(nums):
+    """Solution to the problem described in http://codingbat.com/prob/p108886
+
+    Ex:
+    >>> sum67([1, 2, 2])
+    5
+    >>> sum67([1, 2, 2, 6, 99, 99, 7])
+    5
+    >>> sum67([1, 1, 6, 7, 2])
+    4
+
+    :param nums: list
+    :return: int
+    """
+
+    def between(n, not_between):
+        if n == 7:
+            return 0, not_between
+        return 0, between
+
+    def not_between(n):
+        if n == 6:
+            return 0, between
+        return n, not_between
+
+    between = partial(between, not_between=not_between)
+
+    current_state = not_between
+    sum = 0
+    for n in nums:
+        n_or_zero, current_state = current_state(n)
+        sum += n_or_zero
     return sum
