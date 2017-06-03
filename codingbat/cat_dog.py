af5e2f29b341cdc80e5b427097cb32a95158dcae
renzon <renzon@gmail.com>
2017-06-03 01:50:57 -0300

    Inserted tests for cat_dog problemˆ

---
+++ codingbat/cat_dog.py
@@ -0,0 +1,11 @@
+def cat_dog():
+    """Solution of problem at http://codingbat.com/prob/p164876
+
+    >>> cat_dog('catdog')
+    True
+    >>> cat_dog('catcat')
+    False
+    >>> cat_dog('1cat1cadodog')
+    True
+
+    """
