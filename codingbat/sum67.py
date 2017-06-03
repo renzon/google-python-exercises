da7fe7d310f1298cea7382e6a48db6b3ee93e43f
renzon <renzon@gmail.com>
2017-06-03 01:03:26 -0300

    Added test present on page ˆ

---
+++ codingbat/sum67.py
@@ -0,0 +1,17 @@
+
+
+
+def sum67(nums):
+    """Solution to the problem described in http://codingbat.com/prob/p108886
+
+    Ex:
+    >>> sum67([1, 2, 2])
+    5
+    >>> sum67([1, 2, 2, 6, 99, 99, 7])
+    5
+    >>> sum67([1, 1, 6, 7, 2])
+    4
+
+    :param nums: list
+    :return: int
+    """
