d9757bed7786bd64e1031ae965dccbe035bd5e04
renzon <renzon@gmail.com>
2017-06-03 01:13:28 -0300

    First solution to sum67

--- codingbat/sum67.py
+++ codingbat/sum67.py
@@ -1,6 +1,3 @@
-
-
-
 def sum67(nums):
     """Solution to the problem described in http://codingbat.com/prob/p108886
 
@@ -15,3 +12,15 @@ def sum67(nums):
     :param nums: list
     :return: int
     """
+    sum = 0
+    between_6_and_7 = False
+
+    for n in nums:
+        if n == 6:
+            between_6_and_7 = True
+        if not between_6_and_7:
+            sum += n
+        elif n == 7:
+            between_6_and_7 = False
+
+    return sum
