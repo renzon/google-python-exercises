005d662eb1dc654866b66a1a35255b7168be085a
renzon <renzon@gmail.com>
2017-06-03 01:56:36 -0300

    first cat_dog solution

--- codingbat/cat_dog.py
+++ codingbat/cat_dog.py
@@ -1,4 +1,7 @@
-def cat_dog():
+from collections import deque
+
+
+def cat_dog(s):
     """Solution of problem at http://codingbat.com/prob/p164876
 
     >>> cat_dog('catdog')
@@ -9,3 +12,14 @@ def cat_dog():
     True
 
     """
+    last_3_chars = deque(maxlen=3)
+    CAT_DEQUE = deque('cat')
+    DOG_DEQUE = deque('dog')
+    count = 0
+    for c in s:
+        last_3_chars.append(c)
+        if last_3_chars == CAT_DEQUE:
+            count += 1
+        elif last_3_chars == DOG_DEQUE:
+            count -= 1
+    return count == 0
