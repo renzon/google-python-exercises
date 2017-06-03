56b18b67ae1be1487b531602b6621dcaeebd01c7
renzon <renzon@gmail.com>
2017-06-03 02:06:47 -0300

    third solution to cat_dog

--- codingbat/cat_dog.py
+++ codingbat/cat_dog.py
@@ -23,3 +23,50 @@ def cat_dog(s):
         elif last_3_chars == dog:
             count -= 1
     return count == 0
+
+
+def cat_dog(s):
+    """Solution of problem at http://codingbat.com/prob/p164876
+
+    >>> cat_dog('catdog')
+    True
+    >>> cat_dog('catcat')
+    False
+    >>> cat_dog('1cat1cadodog')
+    True
+
+    """
+    last_3_chars = deque(maxlen=3)
+    cat = deque('cat')
+    dog = deque('dog')
+
+    def choose_0_1_or_minus_1(c):
+        last_3_chars.append(c)
+        if last_3_chars == cat:
+            return 1
+        elif last_3_chars == dog:
+            return -1
+        return 0
+
+    return 0 == sum(map(choose_0_1_or_minus_1, s))
+
+
+def cat_dog(s):
+    """Solution of problem at http://codingbat.com/prob/p164876
+
+    >>> cat_dog('catdog')
+    True
+    >>> cat_dog('catcat')
+    False
+    >>> cat_dog('1cat1cadodog')
+    True
+
+    """
+    last_3_chars = deque(maxlen=3)
+    dct = dict(cat=1, dog=-1)
+
+    def choose_0_1_or_minus_1(c):
+        last_3_chars.append(c)
+        return dct.get(''.join(last_3_chars), 0)
+
+    return 0 == sum(map(choose_0_1_or_minus_1, s))
